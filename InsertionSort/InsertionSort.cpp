cf657c78913220e85327cc8cfce717c053213cf3
Deepak Suthar <suthardeepak23086@gmail.com>
2022-10-17 18:25:36 +0530

    sorting

---
+++ InsertionSort/InsertionSort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int size;
+
+    cout << "Enter Array Size => ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter Array Element " << endl;
+
+    for(int i=0; i<=size; i++){
+        int element;
+        cin >> element;
+        arr[i] = element;
+    }
+    for(int i=1; i<=size; i++){
+
+        int empty_index = i;
+        
+        int currentValue = arr[i];
+
+        for(int j=i-1; j>=0; j--){
+
+            if(arr[j] > currentValue){
+
+                arr[j+1] = arr[j];
+
+                empty_index = j;
+            }
+        }
+        arr[empty_index] = currentValue;
+    }
+    cout << "After Array => {";
+
+    for(int i=0; i<=size; i++){
+         
+        int exit =1;
+
+        cout << arr[i];
+
+        if(i<=size){
+
+            if(i==size){
+                exit=0;
+            }else{
+                cout << ",";
+            }
+        }
+    }
+    cout << "}";
+
+    return 0;
+}
---
+++ SlectionSort/SlectionSort.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int *a, int *b){
+    int temp = *a;
+        *a = *b;
+        *b = temp;
+}
+
+int main(){
+
+    int size, exit = 1;
+     
+    cout << "Size of array => ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter array element => " << endl;
+
+    for(int i=0; i<=size; i++){
+        int element;
+        cin >> element;
+        arr[i]=element;   
+    }
+    for(int step=0; step<=size; step++){
+
+        int min_value_at_index = step;
+
+        for(int i=step; i<=size; i++)
+            if(arr[min_value_at_index]>arr[i]){
+
+                min_value_at_index = i;
+            }
+            swap(&arr[min_value_at_index], &arr[step]);
+        }
+    cout << "After Array => " << "{";
+
+    for(int i=0; i<=size; i++){
+   
+        cout << arr[i];
+        
+        if(i<=size){
+            if(i==size){
+                exit=0;
+            }else{
+                cout << ", ";
+            }
+        }
+    }
+    cout << "}" << endl;
+
+    return 0;
+}
