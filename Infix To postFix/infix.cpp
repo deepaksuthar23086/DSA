06c2af2eee53523daafe11ce2e5548ed89e92ba6
Deepak Suthar <109045248+deepaksuthar23086@users.noreply.github.com>
2023-01-25 15:23:17 +0530

    basic edit

--- Infix To postFix/infix.cpp
+++ Infix To postFix/infix.cpp
@@ -1,81 +1,80 @@
+
 #include <iostream>
 #include <stack>
 using namespace std;
 
 bool isOperand(char ch)
 {
-    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+        return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
 }
 
 int precedence(char op)
 {
-    if (op == '+' || op == '-')
-    {
-        return 1;
-    }
-    else if (op == '*' || op == '/')
-    {
-        return 2;
-    }
-    else
-    {
-        return -1;
-    }
+        if (op == '+' || op == '-')
+        {
+                return 1;
+        }
+        else if (op == '*' || op == '/')
+        {
+                return 2;
+        }
+        else
+        {
+                return -1;
+        }
 }
 
 int main()
 {
+        string postfixExpresion, infixExpresion;
+        stack<char> st;
 
-    string infixExpresion;
-    string postfixExpresion;
-    stack<char> st;
+        char multipleTime = 'y';
 
-    char multipleTime = 'y';
-
-    while (multipleTime == 'y')
-    {
-        cout << "Enter infix Expresion --> ";
-        cin >> infixExpresion;
-
-        for (int i = 0; i <= infixExpresion.size(); i++)
+        while (multipleTime == 'y')
         {
-            int current = infixExpresion[i];
-            if (isOperand(current))
-            {
-                postfixExpresion += current;
-            }
-            else if (current == '(')
-            {
-                st.push(current);
-            }
-            else if (current == ')')
-            {
-                while (st.top() != '(')
-                {
-                    postfixExpresion += st.top();
-                    st.pop();
-                }
-                st.pop();
-            }
-            else
-            {
-                while (!st.empty() && precedence(st.top()) > precedence(current))
-                {
-                    postfixExpresion += st.top();
-                    st.pop();
-                }
-                st.push(current);
-            }
-        }
-        while (!st.empty())
-        {
-            postfixExpresion += st.top();
-            st.pop();
-        }
+                cout << "Enter infix Expresion --> ";
+                cin >> infixExpresion;
 
-        cout << "Postfix Expresion --> " << postfixExpresion << endl;
-        cout << "Want to decide (y/n) --> ";
-        cin >> multipleTime;
-        postfixExpresion ="";
-    }
+                for (int i = 0; i <= infixExpresion.size(); i++)
+                {
+                        int current = infixExpresion[i];
+                        if (isOperand(current))
+                        {
+                                postfixExpresion += current;
+                        }
+                        else if (current == '(')
+                        {
+                                st.push(current);
+                        }
+                        else if (current == ')')
+                        {
+                                while (!st.empty() && st.top() != '(')
+                                {
+                                        postfixExpresion += st.top();
+                                        st.pop();
+                                }
+                                st.pop();
+                        }
+                        else
+                        {
+                                while (!st.empty() && precedence(st.top()) >= precedence(current))
+                                {
+                                        postfixExpresion += st.top();
+                                        st.pop();
+                                }
+                                st.push(current);
+                        }
+                }
+                while (!st.empty())
+                {
+                        postfixExpresion += st.top();
+                        st.pop();
+                }
+
+                cout << "Postfix Expresion --> " << postfixExpresion << endl;
+                cout << "Want to decide (y/n) --> ";
+                cin >> multipleTime;
+                postfixExpresion = "";
+        }
 }
