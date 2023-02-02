b8340337a977853db5917a7f47e7ae300fe43e3e
deepaksuthar23086 <suthardeepak23086@gmail.com>
2023-02-02 17:20:37 +0530

    new progream

--- C++/Bubble Sort/bubblesort.cpp
+++ C++/Bubble Sort/bubblesort.cpp
@@ -1,58 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int sort, int long)
+void bubbleSort(int arr[], int first, int last)
 {
-
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < last; i++)
         {
-
-                for (int j = 0; j < 6 - i; j++)
+                for (int j = 0; j < last - i; j++)
                 {
-
                         if (arr[j] > arr[j + 1])
                         {
-
                                 int temp = arr[j];
-
                                 arr[j] = arr[j + 1];
-
                                 arr[j + 1] = temp;
                         }
                 }
         }
 }
+void printArray(int arr[], int size)
+{
+        for (int i = 0; i <= size; i++)
+        {
+                cout << arr[i];
+                if (i < 5)
+                {
+                        cout << ", ";
+                }
+        }
+        cout << "}" << endl;
+};
 int main()
 {
-
-        int arr[6] = {6, 2, 5, 3, 4, 1};
-
-        cout << "Befor Array => {";
-
-        for (int i = 0; i < size(arr); i++)
-        {
-
-                cout << arr[i];
-
-                if (i < 5)
-                {
-                        cout << ", ";
-                }
-        }
-        cout << "}" << endl;
-
-        bubbleSort(arr, 0, 6);
-
-        cout << "After Array => {";
-
-        for (int i = 0; i < size(arr); i++)
-        {
-
-                cout << arr[i];
-                if (i < 5)
-                {
-                        cout << ", ";
-                }
-        }
-        cout << "}" << endl;
+        int arr[] = {6, 2, 5, 3, 4, 1};
+        int len = size(arr) - 1;
+        cout << "Befor Array --> {";
+        printArray(arr, len);
+        bubbleSort(arr, 0, len);
+        cout << "After Array --> {";
+        printArray(arr, len);
 }
--- C++/Heap Sort/HeapSort.cpp
+++ C++/Heap Sort/HeapSort.cpp
@@ -1,51 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printArray(int arr[], int size){
-    for (int i=0; i<size-1; i++){
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
         cout << arr[i];
-        if(i<size-2){
+        if (i < size - 2)
+        {
             cout << ", ";
         }
     }
     cout << "}" << endl;
 }
-void swap(int *a, int *b){
+void swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-void heapFiy(int arr[], int size, int high){
+void heapFiy(int arr[], int size, int high)
+{
     int larger = high;
     int left = 2 * high + 1;
     int right = 2 * high + 2;
 
-    if(left < size && arr[left] > arr[larger]){
+    if (left < size && arr[left] > arr[larger])
+    {
         larger = left;
     }
-    if(right < size && arr[right]>arr[larger]){
+    if (right < size && arr[right] > arr[larger])
+    {
         larger = right;
     }
-    if(larger != high){
+    if (larger != high)
+    {
         swap(&arr[larger], &arr[high]);
         heapFiy(arr, size, larger);
     }
 }
-void heapSort(int arr[], int size){
-    for (int i=size/2-1; i>=0; i--){
+void heapSort(int arr[], int size)
+{
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
         heapFiy(arr, size, i);
     }
-    for(int i=size-1; i>=0; i--){
+    for (int i = size - 1; i >= 0; i--)
+    {
         swap(&arr[i], &arr[0]);
         heapFiy(arr, i, 0);
     }
-    cout << "After Array => {";
+    cout << "After Array --> {";
     printArray(arr, size);
 }
-int main(){
+int main()
+{
 
-    int arr[]={12,11,13,4,3,6};
-    cout << "Before Array => {";
+    int arr[] = {12, 11, 13, 4, 3, 6};
+    cout << "Before Array --> {";
     printArray(arr, 6);
     heapSort(arr, 6);
 }
